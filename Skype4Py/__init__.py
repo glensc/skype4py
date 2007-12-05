e77b609ea9e95e94b14460820ae6f27e082bcb68
Arkadiusz Wahlig <arkadiusz.wahlig@gmail.com>
2007-12-05 10:39:25 +0000

    Version bump.

--- Skype4Py/__init__.py
+++ Skype4Py/__init__.py
@@ -59,6 +59,6 @@ from errors import ISkypeError as SkypeError, ISkypeAPIError as SkypeAPIError
 from enums import *
 
 
-__version__ = '0.9.28.6'
+__version__ = '0.9.28.7'
 '''The version of Skype4Py.'''
 
