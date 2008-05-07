245e5b59eb83663a9ce92507a2921709a060b219
Arkadiusz Wahlig <arkadiusz.wahlig@gmail.com>
2008-05-07 11:54:23 +0000

    Recently added SetForegroundWindow() calls have been limited to specific situations only. Doing them always caused flashing of windowed Skype4Py apps.

--- Skype4Py/API/windows.py
+++ Skype4Py/API/windows.py
@@ -70,6 +70,7 @@ class _ISkypeAPI(_ISkypeAPIBase):
         self.RegisterHandler(handler)
         self._SkypeControlAPIDiscover = windll.user32.RegisterWindowMessageA('SkypeControlAPIDiscover')
         self._SkypeControlAPIAttach = windll.user32.RegisterWindowMessageA('SkypeControlAPIAttach')
+        windll.user32.GetWindowLongA.restype = c_ulong
         self.start()
         # wait till the thread initializes
         while not self.hwnd:
@@ -106,15 +107,30 @@ class _ISkypeAPI(_ISkypeAPIBase):
         if self.Skype:
             self.SendCommand(ICommand(-1, 'NAME %s' % FriendlyName))
 
+    def GetForegroundWindow(self):
+        fhwnd = windll.user32.GetForegroundWindow()
+        if fhwnd:
+            # awahlig (7.05.2008):
+            # I've found at least one app (RocketDock) that had window style 8 set.
+            # This is odd since windows header files do not contain such a style.
+            # Doing message exchange while this window is a foreground one, causes
+            # lockups if some operations on client UI are involved (for example
+            # sending a 'FOCUS' command). Therefore, we will set our window as
+            # the foreground one for the transmission time.
+            if windll.user32.GetWindowLongA(fhwnd, -16) & 8 == 0:
+                fhwnd = None
+        return fhwnd
+        
     def Attach(self, Timeout=30000, Wait=True):
         if self.Skype:
             return
         if not self.isAlive():
             raise ISkypeAPIError('Skype API closed')
         self.DebugPrint('->', 'SkypeControlAPIDiscover')
-        fhwnd = windll.user32.GetForegroundWindow()
+        fhwnd = self.GetForegroundWindow()
         try:
-            windll.user32.SetForegroundWindow(self.hwnd)
+            if fhwnd:
+                windll.user32.SetForegroundWindow(self.hwnd)
             if not windll.user32.SendMessageTimeoutA(_HWND_BROADCAST, self._SkypeControlAPIDiscover,
                                                      self.hwnd, None, 2, 5000, None):
                 raise ISkypeAPIError('Could not broadcast Skype discover message')
@@ -139,7 +155,8 @@ class _ISkypeAPI(_ISkypeAPIBase):
                 time.sleep(0.01)
             t.cancel()
         finally:
-            windll.user32.SetForegroundWindow(fhwnd)
+            if fhwnd:
+                windll.user32.SetForegroundWindow(fhwnd)
         # check if we got the Skype window's hwnd
         if self.Skype:
             self.SendCommand(ICommand(-1, 'PROTOCOL %s' % self.Protocol))
@@ -170,24 +187,28 @@ class _ISkypeAPI(_ISkypeAPIBase):
             args.append('/MINIMIZED')
         if Nosplash:
             args.append('/NOSPLASH')
-        fhwnd = windll.user32.GetForegroundWindow()
         try:
             if self.hwnd:
-                windll.user32.SetForegroundWindow(self.hwnd)
+                fhwnd = self.GetForegroundWindow()
+                if fhwnd:
+                    windll.user32.SetForegroundWindow(self.hwnd)
             if windll.shell32.ShellExecuteA(None, 'open', self.get_skype_path(), ' '.join(args), None, 0) <= 32:
                 raise ISkypeAPIError('Could not start Skype')
         finally:
-            windll.user32.SetForegroundWindow(fhwnd)
+            if self.hwnd and fhwnd:
+                windll.user32.SetForegroundWindow(fhwnd)
         
     def Shutdown(self):
-        fhwnd = windll.user32.GetForegroundWindow()
         try:
             if self.hwnd:
-                windll.user32.SetForegroundWindow(self.hwnd)
+                fhwnd = self.GetForegroundWindow()
+                if fhwnd:
+                    windll.user32.SetForegroundWindow(self.hwnd)
             if windll.shell32.ShellExecuteA(None, 'open', self.get_skype_path(), '/SHUTDOWN', None, 0) <= 32:
                 raise ISkypeAPIError('Could not shutdown Skype')
         finally:
-            windll.user32.SetForegroundWindow(fhwnd)
+            if self.hwnd and fhwnd:
+                windll.user32.SetForegroundWindow(fhwnd)
 
     def CreateWindow(self):
         # window class has to be saved as property to keep reference to self.WinProc
@@ -270,9 +291,10 @@ class _ISkypeAPI(_ISkypeAPIBase):
             else:
                 Command._timer = timer = threading.Timer(Command.Timeout / 1000.0, self.CommandsStackPop, (Command.Id,))
             self.DebugPrint('->', repr(com))
-            fhwnd = windll.user32.GetForegroundWindow()
+            fhwnd = self.GetForegroundWindow()
             try:
-                windll.user32.SetForegroundWindow(self.hwnd)
+                if fhwnd:
+                    windll.user32.SetForegroundWindow(self.hwnd)
                 if windll.user32.SendMessageA(self.Skype, _WM_COPYDATA, self.hwnd, byref(copydata)):
                     if Command.Blocking:
                         event.wait(Command.Timeout / 1000.0)
@@ -286,6 +308,7 @@ class _ISkypeAPI(_ISkypeAPIBase):
                     self.Skype = None
                     # let the loop go back and try to reattach but only once
             finally:
-                windll.user32.SetForegroundWindow(fhwnd)
+                if fhwnd:
+                    windll.user32.SetForegroundWindow(fhwnd)
         else:
             raise ISkypeAPIError('Skype API error, check if Skype wasn\'t closed')
